5c97f07f89b6988cd7e29bc29053b88e61c4522d
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-18 12:03:15 +0430

    Add associtions and method into comment table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+  belongs_to :author,class_name: 'User'
+  belongs_to :post
+  after_save :update_comment_counter
+  after_destroy :update_comment_counter
+  def update_comment_counter
+    post.update(comment_count: post.comments.count)
+  end
 end
+
