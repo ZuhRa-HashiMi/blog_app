b39ad4d083d038f67e2961015cab163fd518c146
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-18 12:04:05 +0430

    Add associations and method into post table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+  after_save :update_post_counter
+  after_destroy :update_post_counter
+  def update_post_counter
+    author.update(post_count: author.posts.count)
+  end
+
+  def get_recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
