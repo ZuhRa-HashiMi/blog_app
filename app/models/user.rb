c14a36539d3dddcd6e4b551bfb5b2adf317feeb7
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-18 12:00:57 +0430

    Add associations and methode into user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    belongs_to :author,class_name: 'User'
+    belongs_to :post
+    after_save :update_comment_counter
+    after_destroy :update_comment_counter
+    def update_comment_counter
+      post.update(comment_count: post.comments.count)
+    end
 end
