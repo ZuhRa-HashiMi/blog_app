ea054f961463f80af056d224873d31ed574e229f
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-18 19:17:40 +0430

    Add test for comment section

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:author).class_name('User') }
+    it { should belong_to(:post) }
+  end
+
+  describe 'update_post_comments_counter' do
+    let!(:user) { User.create!(name: 'John Doe', email: 'jhone@doe.com', password: '123456', post_counter: 0) }
+    let!(:post) { Post.create!(title: 'My First Post', author: user, comments_counter: 0, likes_counter: 0) }
+    let!(:comment) { Comment.new(author: user, post:) }
+
+    it 'increments the post comment counter after saving' do
+      expect { comment.save }.to change { post.reload.comments_counter }.by(1)
+    end
+  end
 end
