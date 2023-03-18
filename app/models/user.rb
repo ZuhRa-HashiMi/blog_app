c44ce1cbb9601edd6d117ad08a9b027c930e3b99
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-18 12:05:04 +0430

    Add association and method into user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-    belongs_to :author,class_name: 'User'
-    belongs_to :post
-    after_save :update_comment_counter
-    after_destroy :update_comment_counter
-    def update_comment_counter
-      post.update(comment_count: post.comments.count)
-    end
+  has_many :Posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+  def get_recent_post(limit = 3)
+    posts.order(create_at: :desc).limit(limit)
+  end
 end
