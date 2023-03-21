f817e583260eddb9777ec3bc6ae8cda9ecf4a87a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-21 19:13:25 +0430

    Add two contrller user and post

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UserController < ApplicationController
+    def index
+        @post = posts.find_by(author_id: params[:id])
+    end
+
+    def show 
+        @user = posts.find_by(author_id: params[:id], :id params[:id])
+    end
+end
