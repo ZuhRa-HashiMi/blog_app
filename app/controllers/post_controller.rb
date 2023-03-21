7d9ec308fbe1391c877775c1208ebf8bbb21f666
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-21 19:42:23 +0430

    Fix the linter

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,8 +2,7 @@ class PostController < ApplicationController
   def index
     @post = posts.find_by(author_id: params[:id])
   end
-
-  def show 
+  def show
     @user = posts.find_by(author_id: params[:id])
   end
 end
