2faf5fa121d27fc0c350b04e03ce82a2dfcf986b
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-25 17:40:26 +0430

    Add update_coomments_counter method into comment class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post = Post.where(author_id: @user.id).order(created_at: :desc)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
   belongs_to :post
-
-  after_save :update_counters
-  def update_counters
-    author.update(posts_counter: author.posts.count)
-  end
-
-  def most_recent_comments
-    comments.limit(5).order(created_at: :desc)
+  after_save :update_comments_counter
+  
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
