72c811b91b6c2208947ddb08f53a5dde3d47caa8
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-25 17:43:32 +0430

    Add user.increment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', inverse_of: :posts
+  belongs_to :user
   has_many :comments
   has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_counters
   def update_counters
-    author.update(posts_counter: author.posts.count)
+    user.increment!(:posts_counter)
   end
 
   def recent_comments(limit = 5)
