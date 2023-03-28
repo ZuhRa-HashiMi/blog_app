cf2d15cb13bd710be86f6ac09b75781688ca5bc8
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-29 00:07:49 +0430

    Add test for I can see the user's profile picture.

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature "UserShows", type: :feature do
-
+RSpec.describe "Users", type: :system do
+  fixtures :users
+  let(:user1) { users(:one)}
+  let(:user2) { users(:two)}
+  let(:user3) { users(:three)}
+  describe 'show page ' do 
+    it 'I can see the users profile picture.' do
+      visit users_path(user1)
+      expect(page).to have_selector("img[src='#{user1.photo}']")
+      visit users_path(user2)
+      expect(page).to have_selector("img[src='#{user2.photo}']")
+      visit users_path(user3)
+      expect(page).to have_selector("img[src='#{user3.photo}']")
+    end
+  end
 end
