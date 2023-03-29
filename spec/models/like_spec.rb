e04a29f249661595560ab1787f219b0667582a2a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-29 15:46:42 +0430

    Resolve the linter errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   describe 'associations'  do  
-		it { should belong_to(:user).class_name('User') }
-		it { should belong_to(:post) }
-	end
+    it { should belong_to(:user).class_name('User') }
+    it { should belong_to(:post) }
+  end
 
-	describe 'update_post_likes_counter' do
-		let!(:user) { User.create!(name: 'John Doe', posts_counter: 0) }
-		let!(:post) { Post.create!(title: 'My First Post', user:, text: 'this is my first post', comments_counter: 0, likes_counter: 0) }
-		let!(:like) { Like.new(user:, post:) }
+  describe 'update_post_likes_counter' do
+    let!(:user) { User.create!(name: 'John Doe', posts_counter: 0) }
+    let!(:post) { Post.create!(title: 'My First Post', user:, text: 'this is my first post', comments_counter: 0, likes_counter: 0) }
+    let!(:like) { Like.new(user:, post:) }
 
-		it 'increments the post likes counter after saving' do
-			expect { like.save }.to change { post.reload.likes_counter }.by(1)
-		end
-	end
+    it 'increments the post likes counter after saving' do
+      expect { like.save }.to change { post.reload.likes_counter }.by(1)
+    end
+  end
 end
