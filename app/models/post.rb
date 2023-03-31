db59f68a93a58d9b970ed5a0031f53384d19c5dd
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-31 14:54:04 +0430

    Add decrement method

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,9 +11,14 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_counters
+  after_destroy :update_counters
 
   def update_counters
     user.increment!(:posts_counter)
+  end
+
+  def update_counters
+    user.decrement!(:posts_counter)
   end
 
   def most_recent_comments
