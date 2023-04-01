49f3685480bfb5f02773d287834d4bb4c693e5a1
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-01 14:49:26 +0430

    Create user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+  def index
+    @users = User.all
+    
+    if @users
+      render json: @users
+    else
+      render json: { error: 'Users not found'}
+    end
+  end
+    
+  def show
+
+    if @user
+      render json: @user
+    else
+      render json: { error: 'User not found'}
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
