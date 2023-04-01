55dd04409e5adc1a60c8429aba73241d94d10949
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-01 14:54:42 +0430

    Solve the linter errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,20 +2,18 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show]
   def index
     @users = User.all
-    
     if @users
       render json: @users
     else
-      render json: { error: 'Users not found'}
+      render json: { error: 'Users not found' }
     end
   end
-    
-  def show
 
+  def show
     if @user
       render json: @user
     else
-      render json: { error: 'User not found'}
+      render json: { error: 'User not found' }
     end
   end
 
